12861c9724b485e827a38d38efe00b2a3e7acb51
ulapod02 <ulapod02@icloud.com>
2022-03-30 16:31:50 +0400

    lab3_complex

---
+++ JewelryStore/JewelryStoreDatabaseImplement/Models/Warehouse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JewelryStoreDatabaseImplement.Models
+{
+    public class Warehouse
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string WarehouseName { get; set; }
+
+        [Required]
+        public string ResponsibleFullName { get; set; }
+
+        [Required]
+        public DateTime DateCreate { get; set; }
+
+        [ForeignKey("WarehouseId")]
+        public virtual List<WarehouseComponent> WarehouseComponents { get; set; }
+    }
+}
